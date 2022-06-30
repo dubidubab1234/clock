276872cfb14a3a97e44caa852b3d69626724d4a3
dubidubab1234 <lsbmoon78@gmail.com>
2022-07-01 08:14:01 +0900

    new clock

---
+++ index.js
@@ -0,0 +1,18 @@
+const clock = document.getElementById("clock");
+
+const yoils = ["일", "월", "화", "수", "목", "금", "토"];
+
+function working() {
+  const date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  let day = date.getDate();
+  let yoil = date.getDay();
+  let hour = String(date.getHours()).padStart(2, "0");
+  let minute = String(date.getMinutes()).padStart(2, "0");
+  let second = String(date.getSeconds()).padStart(2, "0");
+  clock.innerHTML = `${year}. ${month}. ${day}.(${yoils[yoil]}) ${hour}:${minute}:${second}`;
+}
+
+working();
+setInterval(working, 1000);
